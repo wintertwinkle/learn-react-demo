2121bc4be0f8e1f989632ed04b3bc2f848c90809
yubin <yubin1996@foxmail.com>
2022-03-18 12:51:05 +0800

    learn Forms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 // import Welcome from "./components/Welcome"
-import Clock from "./components/Clock"
-import LoginControl from "./components/LoginControl"
-import Mailbox from "./components/Mailbox"
-import Page from "./components/Page"
-import List from "./components/List"
-import Blog from "./components/Blog"
+// import Clock from "./components/Clock"
+// import LoginControl from "./components/LoginControl"
+// import Mailbox from "./components/Mailbox"
+// import Page from "./components/Page"
+// import List from "./components/List"
+// import Blog from "./components/Blog"
+// import NameForm from "./components/NameForm"
+// import EssayForm from "./components/EssayForm"
+import FlavorForm from "./components/FlavorForm"
 
-const messages = ["React", "Vue", "Angular"]
-const posts = [
-  { id: 1, title: "Hello World", content: "Welcome to learning React!" },
-  { id: 2, title: "Installation", content: "You can install React from npm." },
-]
+// const messages = ["React", "Vue", "Angular"]
+// const posts = [
+//   { id: 1, title: "Hello World", content: "Welcome to learning React!" },
+//   { id: 2, title: "Installation", content: "You can install React from npm." },
+// ]
 
 export default function App() {
   return (
     <div>
-      <LoginControl isLoggedIn={false} />
+      {/* <LoginControl isLoggedIn={false} />
       <Mailbox unreadMessages={messages} />
       <Page />
       <Clock />
       <List items={messages} />
       <Blog posts={posts} />
+      <NameForm />
+      <EssayForm /> */}
+      <FlavorForm />
     </div>
   )
 }
